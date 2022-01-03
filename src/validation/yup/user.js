a9026c440fc27be45e1c767798708223a8d9546d
Konstantin Grigorev <info@a6raywa1cher.com>
2022-01-03 22:32:27 +0300

    Fix password and username validation

--- src/validation/yup/user.js
+++ src/validation/yup/user.js
@@ -14,13 +14,13 @@ yup.setLocale({
 export const emailPasswordSchema = yup
   .object({
     email: yup.string().email().max(200).required(),
-    password: yup.string().min(3).max(128).required(),
+    password: yup.string().min(6).max(128).required(),
   })
   .required();
 
 export const registrationSchema = emailPasswordSchema
   .shape({
-    username: yup.string().min(3).max(16).required(),
+    username: yup.string().min(3).max(25).required(),
     confirmPassword: yup
       .string()
       .oneOf([yup.ref("password")], "Пароли не одинаковы")
